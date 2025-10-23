a7d1c33faa648e30516434f76e91aa8f3e8730a1
Boyd Fletcher <130318010+bcfletcher@users.noreply.github.com>
2025-10-22 23:01:45 -0400

    Add files via upload
    added support for all 7 controllers and dynamic controller detection and animatronic assignment based on controller ID

---
+++ Raspberry PI Pico Code/halloween_pico_code_rev6.py
@@ -0,0 +1,222 @@
+#
+# Halloween Animatronic Automation System
+# This code is using both cores of a Pico or Pico W. Core 1 is running a sensor thread for the KY-008 laser
+# detector via a DaFuRui 5V Relay board. The relay connects to pin14 on the Pico. When someone breaks the
+# beam between the ky-008 laser and detector it trips the relay which activates the p14 on Pico.
+# Then the core_1 kicks off and turns on each of the four SB Components Pico Relay Board (a really cool board!!!) 
+# in series with a sleep between each relay to let the animatronic play.
+#
+# This code also controller an stop light LED board do show status of the controller during boot (yellow),
+# green when waiting for someone to trip the laster and then red once the animatronics start running. 
+#
+# it supports 7 controllers by configuring the animatronics in the animatronicCfg List. the code gets the
+# controller id from the pico and uses that to determine the controller #
+#
+#
+#
+codeRev="6"
+codeDate="22 Oct 2025"
+
+from machine import Pin, lightsleep, PWM
+import _thread
+import time, utime
+
+# configure controller/animatronics pairing
+# 
+#  this is design for our maze which has 7 raspberry pico controllers. 
+#
+# cxr1 is controller #, relay #
+ControllerIDs=["E661A4D4176D6D29",
+               "E6616408438D882B",
+               "E6611C08CB419822",
+               "E661410403206530",
+               "E661385283543132",
+               "E6614104034B2730",
+               "E66358986366412B"]
+#Controller6_ID="E6614104034B2730"
+#Controller7_ID="E66358986366412B"
+animatronicsCfg = [['A/Scary Boy',15, 'B/Scary Girl',10,'C/Clown',10,'D/Ghost',10,"sleep time",0],
+                    ['E/Crawing Zombie',20, 'DD/Ratman',13,'EE/Leatherface',23,'empty_c2s4',1,"sleep time",0],
+                    ['FF/Jump Scare Pumpkin',10,'Skeleton Barrel',10, 'Pumpkin Guy',8,'Witch',8,"sleep time",0],
+                    ['Witch]',7, 'Barry',15,'Werewolf 1',5,'Werewolf 2',5,"sleep time",5],
+                    ['Ghoul',15, 'Bottomless Zombie',15,'Dog House',10,'Headless Skull Guy',7,"sleep time",5],
+                    ['empty_c6s1',10, 'empty_c6s2',10,'empty_c6s3',10,'empty_c6s4',10,"sleep time",5],
+                    ['empty_c7s1',10, 'empty_c7s2',10,'empty_c7s3',10,'empty_c7s4',10,"sleep time",5]]
+
+
+#
+# longest time period of the 4 animatronics. this is used during boot up to make sure
+# all animatronics would and are in steady state before transitioning into sensor mode
+#      
+animatronicsInitialDelay=20
+
+# initialize pins for relays (animatronics)
+relay1 = Pin(18, Pin.OUT)
+relay2 = Pin(19, Pin.OUT)
+relay3 = Pin(20, Pin.OUT)
+relay4 = Pin(21, Pin.OUT)
+
+# Initialize pins for LEDs
+red = PWM(Pin(6), freq=1000)  #  red LED
+yellow = PWM(Pin(7), freq=1000)  #  yellow LED
+green = PWM(Pin(8), freq=1000)  # green LED
+
+# sensor Pin
+sensorPin = Pin(14,Pin.IN, Pin.PULL_UP)
+
+#
+# initialize pins for dip switch - currently not used
+#
+#dipSwitchPin1 = Pin(10, Pin.IN,Pin.PULL_UP)
+#dipSwitchPin2 = Pin(11, Pin.IN,Pin.PULL_UP)
+#dipSwitchPin3 = Pin(12, Pin.IN,Pin.PULL_UP)
+#dipSwitchPin4 = Pin(13, Pin.IN,Pin.PULL_UP)
+
+# thread syncronization global variable
+runAnimatronic=False
+
+#enable debugging
+debug=True
+debug2=False
+
+
+# Function to set the brightness of an LED (0-100%)
+def set_brightness(led, brightness):
+    if brightness < 0 or brightness > 100:
+        raise ValueError("Brightness should be between 0 and 100")
+    led.duty_u16(int(brightness / 100 * 65535))
+
+def allOn():
+    relay1.value(1)
+    relay2.value(1)
+    relay3.value(1)
+    relay4.value(1)
+    
+def allOff():
+    relay1.value(0)
+    relay2.value(0)
+    relay3.value(0)
+    relay4.value(0)
+
+#
+# get's the PICO ID so the code can switch automatically based on the controller.
+#
+# from https://forums.raspberrypi.com/viewtopic.php?t=349189
+#
+def getPicoID():
+    id = ""
+    for b in machine.unique_id():
+      id += "{:02X}".format(b)
+    return(id)
+
+def sensorThread():
+    global runAnimatronic
+    global sensorPin
+    c=0
+    set_brightness(green, 100)
+    set_brightness(red, 0)
+
+    while True:
+         #utime.sleep(20) #remove before use
+        if debug2: print("count=",c," sensor/pin_14=",sensorPin.value())
+        lightsleep(50)
+        c = c + 1
+        if sensorPin.value() == 1:
+            if debug: print("Waiting for animatronics to run")
+            runAnimatronic=True
+            while runAnimatronic == True:
+                pass
+            set_brightness(green, 100)
+            if debug: print("Waiting for sensor detection")
+        else:
+            pass
+        
+def animatronicThread():
+    global runAnimatronic
+    while True:
+
+
+        while runAnimatronic == False:
+            pass
+
+        if debug: print("Run Animatronics")
+
+        if debug: print("Animatronic #1 - ",animatronicsCfg[controllerNumber-1][0],"/",animatronicsCfg[controllerNumber-1][1])
+        relay1.toggle()
+        utime.sleep(animatronicsCfg[controllerNumber-1][1])
+        #
+        # at the end of animatronic #1 turn light to red
+        set_brightness(red, 100)
+        set_brightness(green, 0)
+        allOff()
+        
+        if debug: print("Animatronic #2 - ",animatronicsCfg[controllerNumber-1][2],"/",animatronicsCfg[controllerNumber-1][3])
+        relay2.toggle()
+        utime.sleep(animatronicsCfg[controllerNumber-1][3])
+        allOff()
+        
+        if debug: print("Animatronic #3 - ",animatronicsCfg[controllerNumber-1][4],"/",animatronicsCfg[controllerNumber-1][5])
+        relay3.toggle()
+        utime.sleep(animatronicsCfg[controllerNumber-1][5])
+        allOff()
+        
+        if debug: print("Animatronic #4 - ",animatronicsCfg[controllerNumber-1][6],"/",animatronicsCfg[controllerNumber-1][7])
+        relay4.toggle()
+        utime.sleep(animatronicsCfg[controllerNumber-1][7])
+
+        # extra time to make sure all animatronics are done so they can restart correctly
+        if debug: print("Sleep Time - ",animatronicsCfg[controllerNumber-1][8],"/",animatronicsCfg[controllerNumber-1][9])
+        utime.sleep(animatronicsCfg[controllerNumber-1][9])
+
+        allOff()    
+        set_brightness(red, 0)
+        if debug: print("Animatronics Run Done")
+        runAnimatronic=False
+
+
+
+
+
+#
+# main code
+# 
+
+if debug: print("Initializing System, please wait...")
+if debug: print("Code Rev: ",codeRev," Code Date: ",codeDate)
+if debug: print("Pico's Unique ID=",getPicoID())
+# change this to reflect which controller above in the list that this instance is supporting
+try: 
+    controllerNumber=ControllerIDs.index(getPicoID())+1
+except:
+    print("Unknown Controller, defaulting to 1")
+    controllerNumber=1
+if debug: print("Controller Number=",controllerNumber)
+if debug: print("Animatronics Configured=",animatronicsCfg[controllerNumber-1])
+if debug: print("Sensor Pin Current Value=",sensorPin.value())
+
+
+set_brightness(green, 100)
+set_brightness(yellow, 100)
+set_brightness(red, 100)
+utime.sleep(2)
+set_brightness(green, 0)
+set_brightness(red, 0)
+if debug: print("All Animatronics will activate to verify function")
+allOff()
+allOn()
+utime.sleep(2)
+allOff()
+sensorPin.value(0)
+runAnimatronic=False
+utime.sleep(animatronicsInitialDelay)
+set_brightness(yellow, 0)
+
+set_brightness(green, 100)
+if debug: print("Start Animatronic Thread")
+animatronic = _thread.start_new_thread(animatronicThread,())
+if debug: print("Start Sensor Thread")
+if debug: print("Controller is now live!")
+sensorThread()
+
+allOff()
+                                                                                 
